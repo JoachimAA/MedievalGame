bb2fe6b31082f13a01be703a583025b9ed02ed92
JoachimAA <joachimarthey@hotmail.co.uk>
2018-10-03 15:27:38 +0100

    first commit

---
+++ Assets/playermovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float movementforce = 500f;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(movementforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-movementforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, movementforce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -movementforce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
